4774451867d25d733430a8a8d8582738e1a43c84
Dovydas-Kr <dovydas.kr@gmail.com>
2015-09-21 16:38:23 +0100

    Update euler1.py

--- euler1.py
+++ euler1.py
@@ -1,7 +1,7 @@
 """ Python code"""
 """ For loop goes trough all the numbers from 1 to 1000. Using if function if i is divisible by 3 or 5.
 If yes then the number 'i' is added to the sum of other numbers that are multiples of 3 and 5. 
-After the loop is finished numbers sum is displayed on the screen.***
+After the loop is finished numbers sum is displayed on the screen."""
 
 sum = 0
 for i in range (1, 1000):
