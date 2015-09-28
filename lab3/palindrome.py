7d41aa39a33936b47ec1c61263a0728aecde624b
Dovydas-Kr <dovydas.kr@gmail.com>
2015-09-28 12:23:27 +0000

    v_2.3

--- lab3/palindrome.py
+++ lab3/palindrome.py
@@ -1,11 +1,14 @@
 # Program that checks if a string is palindrome.
 import string
-
+#Asks for input
 word = raw_input("Enter a string: ")
 
+#changes string to lover case
 word1 = word.lower()
+#reverses string
 word2 = reversed(word1)
 
+#Checks if strings are the same
 if list(word1) == list(word2):
    print("True")
 else:
