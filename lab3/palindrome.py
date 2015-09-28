13ceac226725a84a72ff0eab2f6a32aafa6406d4
Dovydas-Kr <dovydas.kr@gmail.com>
2015-09-28 12:12:04 +0000

    Palindrome program done!

--- lab3/palindrome.py
+++ lab3/palindrome.py
@@ -1,6 +1,12 @@
+import string
 
-
-:q
-:wq
-
-
+# Program that checks if a string is palindrome.
+word = raw_input("Enter a string: ")
+
+word1 = word.lower()
+word2 = reversed(word1)
+
+if list(word1) == list(word2):
+   print("True")
+else:
+   print("False")
