bfab19cffc1a4c656804334cde8bdf73b07c8bb2
Dovydas-Kr <dovydas.kr@gmail.com>
2015-09-28 11:34:39 +0000

    lab 3 folder added

--- euler2.py
+++ euler2.py
@@ -7,3 +7,7 @@ sum = 0
 
 for i in range (0, 4000000):
     sum = term1 + term2
+  sum = term1 + term2
+  sum = term1 + term2sum = term1 + term2
+  sum = term1 + term2sum = term1 + term2sum = term1 + term2sum = term1 + term2
+  sum = term1 + term2
